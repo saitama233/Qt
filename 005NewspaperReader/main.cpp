3c75916f4a699b971b5c678d11b3b8b0f9f069fe
saitama <806126488@qq.com>
2017-12-13 17:10:21 +0800

    自定义信号槽

---
+++ 005NewspaperReader/main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include "newspaper.h"
+#include "reader.h"
+
+//自定义信号槽
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    Newspaper newspaper("Newspaper A");
+    Reader reader;
+    QObject::connect(&newspaper, &Newspaper::newPaper,
+                     &reader, &Reader::receiveNewspaper);
+    newspaper.send();
+
+    return a.exec();
+}
