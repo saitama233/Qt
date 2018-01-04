394ccee99bab88c122fd0cc0f8d5b36860619094
saitama <806126488@qq.com>
2018-01-04 16:49:34 +0800

    017文件对话框 简单的文本文件编辑器，具有打开和保存文件的功能

--- 015QMessageBox/mainwindow.cpp
+++ 015QMessageBox/mainwindow.cpp
@@ -1,8 +1,33 @@
+#include <QMessageBox>
+#include <QDebug>
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    QMessageBox msgBox;
+    msgBox.setText(tr("The document has been modified"));
+    msgBox.setInformativeText(tr("Doyou want to save your changes?"));
+    msgBox.setDetailedText("Differences here...");
+    msgBox.setStandardButtons(QMessageBox::Save
+                               | QMessageBox::Discard
+                               | QMessageBox::Cancel);
+    msgBox.setDefaultButton(QMessageBox::Save);
+    int ret = msgBox.exec();
+    switch (ret) {
+    case QMessageBox::Save:
+        qDebug() << "Save document!";
+        break;
+    case QMessageBox::Discard:
+        qDebug() << "Discard changes!";
+        break;
+    case QMessageBox::Cancel:
+        qDebug() << "Close document!";
+        break;
+    default:
+        break;
+    }
+
 }
 
 MainWindow::~MainWindow()
---
+++ 017TextEditor/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTextEdit>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    void openFile();
+    void saveFile();
+    QAction *openAction;
+    QAction *saveAction;
+    QTextEdit *textEdit;
+};
+
+#endif // MAINWINDOW_H
